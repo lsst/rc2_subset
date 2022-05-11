ffa7a68a697e3b1178b353a5a62ab809d9f773b7
Eli Rykoff <erykoff@stanford.edu>
2022-05-10 17:06:01 -0700

    Split step2 into 2a (multiprocessing) and 2b (no multiprocessing).

--- bin/measureHscRC2Metrics.sh
+++ bin/measureHscRC2Metrics.sh
@@ -4,9 +4,11 @@ set -e
 
 pipetask --long-log run -j $NUMPROC -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep1' -i HSC/RC2/defaults --register-dataset-types -o jenkins/step1
 
-pipetask --long-log run -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep2' -i jenkins/step1 --register-dataset-types -o jenkins/step2
+pipetask --long-log run -j $NUMPROC -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep2a' -i jenkins/step1 --register-dataset-types -o jenkins/step2a
 
-pipetask --long-log run -j $NUMPROC -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep3' -d "tract = 9813 and skymap = 'hsc_rings_v1' AND patch in (40)" -i jenkins/step2 --register-dataset-types -o jenkins/step3
+pipetask --long-log run -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep2b' -i jenkins/step2a --register-dataset-types -o jenkins/step2b
+
+pipetask --long-log run -j $NUMPROC -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep3' -d "tract = 9813 and skymap = 'hsc_rings_v1' AND patch in (40)" -i jenkins/step2b --register-dataset-types -o jenkins/step3
 
 pipetask --long-log run -j $NUMPROC -b ${RC2_SUBSET_DIR}/SMALL_HSC/butler.yaml -p ${RC2_SUBSET_DIR}'/pipelines/DRP.yaml#nightlyStep4' -d "tract = 9813 and skymap = 'hsc_rings_v1' AND patch in (40)" -i jenkins/step3 --register-dataset-types -o jenkins/step4
 
